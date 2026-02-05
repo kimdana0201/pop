9e3b53d8a1fc7a967b1645f0a6701a34fa8869cb
unknown <kimdana0201@gmail.com>
2026-02-05 21:12:59 +0900

    초기화

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 print("C:\\Users\\python\\test.py")
+
+#   1.git --version : 깃이 설치 되었는지 확인하는 코드
+#   2.git init : 현재 폴더를 git 원격저장소와 연결된 로컬 저장소라고 알려주는 역할
+#   3.git add : 로컬 저장소 파일을 1차로 가상 공간에 추가하는 역할(git 원격 저장소와 연결된 폴더로 이동 후)
+#   4.git commit -m "~" : 업로드하는 버전의 상태를 알려주는 코드
+#   5.git push origin master : 로컬저장소에 있는 파일을 원격저장소와 동기화하는 코드
+#   6.git remote -v : 깃허브의 원격저장소와 연결되어 있는 상태를 확인하는 코드
+#   7.git remote add origin "깃허브주소" : 깃허브의 원격저장소를 연결하는 코드
+
+# 3 -> 4 -> 5 순서로 입력   
